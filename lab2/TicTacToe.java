ac6935c519eee1521b58f8340d643079b5bc6c8b
scrable <cruss.seq11@gmail.com>
2016-11-03 12:11:47 -0700

    Change formatting

--- lab2/TicTacToe.java
+++ lab2/TicTacToe.java
@@ -1,202 +1,170 @@
 import java.util.Scanner;
 
-public class TicTacToe{
+public class TicTacToe
+{
+  private char[][] board;
+  private char player; // 'X' or 'O'
 
-	/*public static void main(String[] args) {
+  public TicTacToe()
+  {
+    board = new char[3][3];
+    player = 'X';
+    for (int i = 0; i < 3; i++)
+      for (int j = 0; j < 3; j++)
+        board[i][j] = ' ';
+  }
 
-	}*/
+  public boolean play(String s)
+  {
+    char charColumn = s.charAt(1);
+    int column;
 
-	private char[][] board;
-	private char player; // 'X' or 'O'
+    if (charColumn == '1')
+      column = 0;
+    else if (charColumn == '2')
+      column = 1;
+    else if (charColumn == '3')
+      column = 2;
+    else
+      return false;
 
-	/*
-	 * Instantiate board to be a 3 by 3 char array of spaces.
-	 * Set player to be 'X'.
-	 */
+    if (s.charAt(0) == 'a' || s.charAt(0) == 'A')
+    {
+      if (board[0][column] == ' ')
+      {
+        board[0][column] = player;
+        return true;
+      }
+    }
+    if (s.charAt(0) == 'b' || s.charAt(0) == 'B')
+    {
+      if (board[1][column] == ' ')
+      {
+        board[1][column] = player;
+        return true;
+      }
+    }
+    if (s.charAt(0) == 'c' || s.charAt(0) == 'C')
+    {
+      if (board[2][column] == ' ')
+      {
+        board[2][column] = player;
+        return true;
+      }
+    }
 
-	public TicTacToe() {
-		/*
-		* Step 1: create an empty board, with an initial value
-		* of a space (' ')
-		*/
-		board = new char[3][3];
-		player = 'X';
-		for(int i = 0; i < 3; i++)
-			for(int j = 0; j < 3; j++)
-				board[i][j] = ' ';
-	}
+    return false;
+  }
 
-	/*
-	 * If s represents a valid move, add the current player's symbol to the board and return true.
-	 * Otherwise return false.
-	 */
-	public boolean play(String s) {
-		/* Step 2: Fill in here with your own
-		* play logic, and replace the return with your
-		* own.
-		*/
-		char charColumn = s.charAt(1);
-		int column;
+  public void switchTurn()
+  {
+    if (player == 'X')
+      player = 'O';
+    else
+      player = 'X';
+  }
 
-		if(charColumn == '1')
-			column = 0;
-		else if(charColumn == '2')
-			column = 1;
-		else if(charColumn == '3')
-			column = 2;
-		else return false;
+  public boolean won()
+  {
 
-		if(s.charAt(0) == 'a' || s.charAt(0) == 'A')
-		{
-			if(board[0][column] == ' ')
-			{
-				board[0][column] = player;
-				return true;
-			}
-		}
-		if(s.charAt(0) == 'b' || s.charAt(0) == 'B')
-		{
-			if(board[1][column] == ' ')
-			{
-				board[1][column] = player;
-				return true;
-			}
-		}
-		if(s.charAt(0) == 'c' || s.charAt(0) == 'C')
-		{
-			if(board[2][column] == ' ')
-			{
-				board[2][column] = player;
-				return true;
-			}
-		}
+    // check horizontal win
+    for (int i = 0; i < 3; i++)
+      if (board[i][0] != ' ' && board[i][0] == board[i][1] &&
+          board[i][0] == board[i][2])
+        return true;
 
-	  return false;
-	}
+    // check vertical Wins
+    for (int i = 0; i < 3; i++)
+      if (board[0][i] != ' ' && board[0][i] == board[1][i] &&
+          board[0][i] == board[2][i])
+        return true;
 
-	/*
-	 * Switches the current player from X to O, or O to X.
-	 */
-	public void switchTurn() {
-		// Step 3: Fill in with your code to toggle between
-		// 'X' and 'O'
-		if(player == 'X')
-			player = 'O';
-		else
-			player = 'X';
-	}
+    // check top left to bottom right Wins
+    if (board[0][0] != ' ' && board[0][0] == board[1][1] &&
+        board[0][0] == board[2][2])
+      return true;
+    // check top right to bottom left
+    if (board[0][2] != ' ' && board[0][2] == board[1][1] &&
+        board[0][2] == board[2][0])
+      return true;
 
-	/*
-	 * Returns true if the current player has won the game.
-	 * Three in a row, column or either diagonal.
-	 * Otherwise, return false.
-	 */
-	public boolean won() {
-		/* Step 5: Fill in the code for the won method. This method
-        * should return true if the current player has 3 in-a-row
-		* in any row, column or diagonal. Otherwise, return false.
-		*/
+    return false;
+  }
 
-		//check horizontal win
-		for(int i = 0; i < 3; i++)
-			if(board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][0] == board[i][2])
-				return true;
+  public boolean stalemate()
+  {
+    for (int i = 0; i < 3; i++)
+      for (int j = 0; j < 3; j++)
+        if (board[i][j] == ' ')
+          return false;
+    return true;
+  }
+  public char getPlayer() { return player; }
+  public void print()
+  {
+    System.out.println();
+    System.out.println("\t  1 2 3");
+    System.out.println();
+    System.out.println("\tA " + board[0][0] + "|" + board[0][1] + "|" +
+                       board[0][2]);
+    System.out.println("\t  -----");
+    System.out.println("\tB " + board[1][0] + "|" + board[1][1] + "|" +
+                       board[1][2]);
+    System.out.println("\t  "
+                       + "-----");
+    System.out.println("\tC " + board[2][0] + "|" + board[2][1] + "|" +
+                       board[2][2]);
+    System.out.println();
+  }
 
-		//check vertical Wins
-		for(int i = 0; i < 3; i++)
-			if(board[0][i] != ' ' && board[0][i] == board[1][i] && board[0][i] == board[2][i])
-				return true;
+  public static void main(String[] args)
+  {
 
-		//check top left to bottom right Wins
-		if(board[0][0] != ' ' && board[0][0] == board[1][1] && board[0][0] == board[2][2])
-			return true;
-		//check top right to bottom left
-		if(board[0][2] != ' ' && board[0][2] == board[1][1] && board[0][2] == board[2][0])
-			return true;
+    Scanner in = new Scanner(System.in);
+    TicTacToe game = new TicTacToe();
+    System.out.println("Welcome to tic-tac-toe");
+    System.out.println(
+        "Enter coordinates for your move following the X and O prompts");
 
-		return false; // TODO: replace with your own return statement.
-	}
+    while (!game.stalemate())
+    {
+      // Print the game
+      game.print();
 
-	/*
-	 * Returns true if there are no places left to move
-	 */
-	public boolean stalemate() {
-	    /*
-		 * Step 4: Fill in the code for the stalemate method. It
-         * should return true if there are no more places to move
-		 * on the board. Otherwise, return false return false;
-		 */
-		 for(int i = 0; i < 3; i++)
-			 for(int j = 0; j < 3; j++)
-				 if(board[i][j] == ' ')
-				 	return false;
-		 return true;   // replace with your own return
-	}
-	public char getPlayer() {
-		return player;
-	}
-	public void print() {
-		System.out.println();
-		System.out.println("\t  1 2 3");
-		System.out.println();
-		System.out.println("\tA "+board[0][0]+"|"+board[0][1]+"|"+board[0][2]);
-		System.out.println("\t  -----");
-		System.out.println("\tB "+board[1][0]+"|"+board[1][1]+"|"+board[1][2]);
-		System.out.println("\t  "+"-----");
-		System.out.println("\tC "+board[2][0]+"|"+board[2][1]+"|"+board[2][2]);
-		System.out.println();
-	}
+      // Prompt player for their move
+      System.out.println("Enter your move: ");
+      String move = in.next();
 
-	/*
-	 * Step 6: Main Method for Final Step - Delete your main method
-	 * and uncomment this one.
-	 * Runs the game by getting input from the user, making the
-	 * appropriate moves, and prints who won or if it was a stalemate.
-	*/
+      while (move.length() != 2)
+      {
+        System.out.println("Invalid move. Enter another move: ");
+        move = in.next();
+      }
 
-	public static void main(String[] args) {
+      while (!game.play(move))
+      {
+        System.out.println("Invalid move. Enter another move: ");
+        move = in.next();
+      }
 
-		Scanner in = new Scanner(System.in);
-		TicTacToe game = new TicTacToe();
-		System.out.println("Welcome to tic-tac-toe");
-		System.out.println("Enter coordinates for your move following the X and O prompts");
+      // Loop while the method play does not return true when given their move.
+      // Body of loop should ask for a different move
 
-		while(!game.stalemate()) {
-			//Print the game
-			game.print();
+      // If the game is won, call break;
+      if (game.won())
+        break;
 
-			//Prompt player for their move
-			System.out.println("Enter your move: ");
-			String move = in.next();
-
-			while(move.length() != 2)
-			{
-				System.out.println("Invalid move. Enter another move: ");
-				move = in.next();
-			}
-
-			while(!game.play(move))
-			{
-				System.out.println("Invalid move. Enter another move: ");
-				move = in.next();
-			}
-
-			//Loop while the method play does not return true when given their move.
-			//Body of loop should ask for a different move
-
-			//If the game is won, call break;
-			if(game.won())
-				break;
-
-			//Switch the turn
-			game.switchTurn();
-
-		}
-		game.print();
-		if(game.won()){
-			System.out.println("Player "+game.getPlayer()+" Wins!!!!");
-		} else {
-			System.out.println("Stalemate");
-		}
-	}
+      // Switch the turn
+      game.switchTurn();
+    }
+    game.print();
+    if (game.won())
+    {
+      System.out.println("Player " + game.getPlayer() + " Wins!!!!");
+    }
+    else
+    {
+      System.out.println("Stalemate");
+    }
+  }
 }
